4f1f7202584b6c787758f295d292a46e6954f0a8
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-10-13 16:32:52 +0200

    Add model references

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
 end
-
-# Ello! Again2
-# I am going to start things soon
-# Siehe Rails von Kitt
--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,2 +1,4 @@
 class Consultation < ApplicationRecord
+  belongs_to :user
+  has_one :name
 end
