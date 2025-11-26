140925e70626e3b0634e7fea1cc906bfd32280da
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-11-26 16:40:58 +0100

    Add seeds for bfrandomizer aswell as the model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 puts "Cleaning DB"
 Consultation.destroy_all
 User.delete_all
+Bfrandomizer.destroy_all
 
 puts "Creating Admin"
 User.create!(email: "zuh4rez_gaming_consultancy@gmx.de", password: "password")
@@ -10,5 +11,7 @@ User.create!(email: "Carlo@Carlo.com", password: "password")
 puts "Creating Test-Consultation"
 Consultation.create!(user_id: 2, name: "RPG")
 
+puts "Creating Test Bfrandomiizer"
+Bfrandomizer.create!(name: "SVK")
 
-puts "Finished creating Seeds with #{User.count} Users and #{Consultation.count} Consultation."
+puts "Finished creating Seeds with #{User.count} Users, #{Consultation.count} Consultation and #{Bfrandomizer.count} Randomizers."
