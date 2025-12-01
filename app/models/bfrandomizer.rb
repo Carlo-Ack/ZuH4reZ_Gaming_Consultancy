b0fbbe974ed04756651574b02b9504466074c352
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-12-01 11:27:41 +0100

    Add migration of randomizer content aswell as associations to the randomizer model

--- app/models/bfrandomizer.rb
+++ app/models/bfrandomizer.rb
@@ -1,9 +1,9 @@
 class Bfrandomizer < ApplicationRecord
   belongs_to :user
-  # has_one :name
-  # has_one :primary_weapon
-  # has_one :secondary_weapon
-  # has_many :gadgets
-  # has_one :class
-  # has_one :class_specialization
+  has_one :name
+  has_one :primary_weapon
+  has_one :secondary_weapon
+  has_many :gadgets
+  has_one :class
+  has_one :class_specialization
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_153915) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_01_101729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_153915) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "class"
+    t.string "class_specialization"
+    t.string "primary_weapon"
+    t.string "secondary_weapon"
+    t.string "gadget_one"
+    t.string "gadget_two"
   end
 
   create_table "consultations", force: :cascade do |t|
