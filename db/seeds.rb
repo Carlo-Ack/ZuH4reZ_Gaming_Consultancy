c68806d650bd3304b67f2d631ef41b29f7b518ff
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-15 08:22:20 +0100

    Add first try of reading yml file number two

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,21 +17,26 @@ puts "Creating Data for Bfrandomizer"
 Bfrandomizer.create!(user: test_user, name: "Try Nr. 1", player_class: "Support", class_specialization: "Push", primary_weapon: "SVK", secondary_weapon: "Revolver", gadget_one: "Defi", gadget_two: "Launcher")
 Bfrandomizer.create!(user: test_user, name: "Try Nr. 2", player_class: "Enginner", class_specialization: "Repair", primary_weapon: "DRS", secondary_weapon: "Five-Seven", gadget_one: "Cover", gadget_two: "Anti-Grenade-Device")
 
-# ===================================================================================================
-# Moving forward we need to implement the static data from the game in here somehow
-# That means that the roll the dice method should be able to pick everything randomly - not just one set
+player_class_file_path = Rails.root.join("db", "seeds", "player_classes.yml")
+player_class_data = YAML.load_file(player_class_file_path)
 
-file_path = Rails.root.join("db", "seeds", "player_classes.yml")
-data = YAML.load_file(file_path)
-
-data["player_classes"].each do |player_class|
+player_class_data["player_classes"].each do |player_class|
   Bfrandomizer.find_or_create_by!(
     name: player_class["name"]
-  ) do |bf|
-    bf.description = player_class["description"]
+  ) do |pc|
+    pc.description = player_class["description"]
   end
 end
 
-# ===================================================================================================
+class_specialization_file_path = Rails.root.join("db", "seeds", "class_specialization.yml")
+class_specialization_data = YAML.load_file(class_specialization_file_path)
+
+class_specialization_data["class_specializations"].each do |class_specialization|
+  Bfrandomizer.find_or_create_by!(
+    name: class_specialization["name"]
+  ) do |cs|
+    cs.description = class_specialization["description"]
+  end
+end
 
 puts "Finished creating Seeds with #{User.count} Users, #{Consultation.count} Consultation and #{Bfrandomizer.count} Randomizers."
