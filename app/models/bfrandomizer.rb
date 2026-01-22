c38fc8086bbb197ced6b01e64358d5f7344fe137
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-22 11:25:53 +0100

    Add starting point of model loading yaml data to roll random dice

--- app/models/bfrandomizer.rb
+++ app/models/bfrandomizer.rb
@@ -1,8 +1,21 @@
 class Bfrandomizer < ApplicationRecord
+  require "yaml"
+  
   # belongs_to :user, optional: true
 
-  def self.random
-    order("Random()").first
-  end
+ data_file_path = Rails.root.join("db", "seeds", ["player_classes.yml", "class_specialization.yml"])
+ 
+ def self.random
+  order("Random()").first
+end
+
+data_class_data = YAML.load_file(player_class_file_path)
+
+# Out of my head what should be working is:
+# def self.random
+# player_class.sample
+# class_specialization.sample
+# primary_weapon.sample etc...
+
 
 end
