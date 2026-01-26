0a101050534d1ea0460ca439390e91f7b19f782e
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-26 14:24:02 +0100

    Add first take on new randomizer logic

--- app/models/bfrandomizer.rb
+++ app/models/bfrandomizer.rb
@@ -1,21 +1,27 @@
-class Bfrandomizer < ApplicationRecord
+class Bfrandomizer
   require "yaml"
   
   # belongs_to :user, optional: true
 
- data_file_path = Rails.root.join("db", "seeds", ["player_classes.yml", "class_specialization.yml"])
+ data_file_path = Rails.root.join("config", "gamedata")
  
+ def self.data
+    @data ||= Dir[data_file_path.join("*.yml")].each_with_object({}) do |file, hash|
+      key = File.basename(file, ".yml")
+      hash[key] = YAML.load_file(file)
+    end
+  end
+
  def self.random
-  order("Random()").first
-end
+  player_class = data["player_classes"].sample
 
-data_class_data = YAML.load_file(player_class_file_path)
-
-# Out of my head what should be working is:
-# def self.random
-# player_class.sample
-# class_specialization.sample
-# primary_weapon.sample etc...
-
-
+  BfrandomizerResult.new(
+    player_class: player_class,
+    class_specialization: data["class_specializations"][player_class]&.sample,
+    primary_weapon: data["primary_weapons"].sample,
+    secondary_weapon: data["secondary_weapons"].sample,
+    gadget_one: data["gadgets"].sample,
+    gadget_two: data["gadgets"].sample
+  )
+ end
 end
