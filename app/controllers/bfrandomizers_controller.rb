e078ca15ea885988903ad14f1e3cf3c2f4d57545
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-27 15:23:56 +0100

    Add comments for future to do's such as being able to save a randomizer roll

--- app/controllers/bfrandomizers_controller.rb
+++ app/controllers/bfrandomizers_controller.rb
@@ -1,8 +1,9 @@
 class BfrandomizersController < ApplicationController
 
   def index
-    # @bfrandomizer_history = current_user.bfrandomizer
+    # @bfrandomizer_history = current_user.bfrandomizer_results
     @bfrandomizer_history = []
+    # empty array because nothing is saved yet
   end
 
   def new
@@ -13,5 +14,4 @@ class BfrandomizersController < ApplicationController
     @result = Bfrandomizer.random
     render partial: 'bfrandomizers/random_result', locals: { result: @result }
   end
-
 end
--- app/models/bfrandomizer_result.rb
+++ app/models/bfrandomizer_result.rb
@@ -1,13 +1,16 @@
-class BfrandomizerResult
-  attr_reader :player_class, :class_specialization, :primary_weapon,
-              :secondary_weapon, :gadget_one, :gadget_two
+# class BfrandomizerResult
+#   attr_reader :player_class, :class_specialization, :primary_weapon,
+#               :secondary_weapon, :gadget_one, :gadget_two
 
-  def initialize(attrs)
-    @player_class = attrs[:player_class]
-    @class_specialization = attrs[:class_specialization]
-    @primary_weapon = attrs[:primary_weapon]
-    @secondary_weapon = attrs[:secondary_weapon]
-    @gadget_one = attrs[:gadget_one]
-    @gadget_two = attrs[:gadget_two]
-  end
-end
+#   def initialize(attrs)
+#     @player_class = attrs[:player_class]
+#     @class_specialization = attrs[:class_specialization]
+#     @primary_weapon = attrs[:primary_weapon]
+#     @secondary_weapon = attrs[:secondary_weapon]
+#     @gadget_one = attrs[:gadget_one]
+#     @gadget_two = attrs[:gadget_two]
+#   end
+# end
+
+# maybe needed later if I would want to be able to store these rolls into a database.
+# Should be only available if every other terms are met like being logged in as a user.
