1fd851160290a727dfdf800927a3da6fd7864798
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-03-03 12:49:31 +0100

    Refactor of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,29 @@
 Rails.application.routes.draw do
+  # Initial Websites root to home
   root to: "pages#home"
+
+  # Health check 
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Provide complete set of routes for a user
   resources :user
 
+  # Provide route to consultation based of which category chosen
   resources :consultations
   post 'consult/category', to: 'consultations#chosen_category', as: :chosen_category
 
+  # Provide roll route for bfrandomizers to be able to save it later on
+  # maybe later on: :member if its linked to users!
   resources :bfrandomizers do
     get :roll, on: :collection
   end
-  # maybe later on: :member if its linked to users!
   
+  # Provide roll route for huntrandomizers to be able to save it later on 
   resources :huntrandomizers do
     get :roll, on: :collection
   end
 
+  # Provide routes for Impressum and FAQ
   get "impressum", to: "pages#impressum"
   get "faq", to: "pages#faq"
 
